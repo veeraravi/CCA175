bea54f0eaa82d78202cac9e864f92b3b29c8f23d
unknown <email2dgk@gmail.com>
2017-01-16 23:38:32 -0500

    Spark AggregateByKey() examples

---
+++ 02 transform, stage and store/spark/find_average_using_reduceByKey.scala
@@ -0,0 +1,11 @@
+val prodFile = "/user/email2dgk/review/spark/data/prod_sales_rows.txt"
+val prodKvRdd = sc.textFile(prodFile).map{x=> val z= x.split(","); (z(0), z(1).toInt)}
+
+val prodCountRdd = prodKvRdd.map{case(p,s) => (p,1)}.reduceByKey(_+_)
+val prodSalesRdd = prodKvRdd.reduceByKey(_+_)
+prodSalesRdd.leftOuterJoin(prodCountRdd)
+
+prodSalesRdd.leftOuterJoin(prodCountRdd).foreach{ 
+case(k,(s,someC))=> val count = someC.fold(1)(_.toInt); 
+val avg = s.toInt/count.toInt; 
+println(k + ":"+ avg)}
